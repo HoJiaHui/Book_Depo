5c89fbeaac26ad4f46c4165926ba223a328be0de
MooMonk <jiahuii.ho@gmail.com>
2020-12-21 00:02:22 +0800

    Welcome Book_Depo

---
+++ lib/bookdetailscreen.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+import 'package:http/http.dart' as http;
+import 'book.dart';
+
+
+class BookDetailScreen extends StatefulWidget {
+  final Book book;
+  const BookDetailScreen({Key key, this.book}) : super(key: key); 
+
+  @override
+  _BookDetailScreenState createState() => _BookDetailScreenState();
+}
+
+class _BookDetailScreenState extends State<BookDetailScreen> {
+  double screenHeight, screenWidth; 
+
+  @override
+  Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
+ 
+ return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.book.booktitle),
+        ),
+      body: Center(
+        child: SingleChildScrollView(
+        child: Column(children: [
+          Container(
+          height: screenHeight / 1.6,
+          width: screenWidth / 1.5,
+          child: CachedNetworkImage(
+          imageUrl:"http://slumberjer.com/bookdepo/bookcover/${widget.book.cover}.jpg",
+          fit: BoxFit.cover,
+          placeholder: (context, url) => new CircularProgressIndicator(),
+          errorWidget: (context, url, error) => new Icon(
+            Icons.broken_image,
+        size: screenWidth / 2,
+ ),
+ )),
+ Container(
+    child: Column(children: [
+      SizedBox(height: 5),
+      Text(widget.book.booktitle,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+      fontSize: 19,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+      )),
+ ]),
+ ),
+Container(
+    child: Column(children: [
+      SizedBox(height: 5),
+      Text(
+      widget.book.author,
+      textAlign: TextAlign.center,
+      style: TextStyle(
+      fontSize: 15,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+      )),
+SizedBox(height: 8),
+      Row(children: [
+          RichText(
+           text: TextSpan(
+              style:
+              TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[              
+              TextSpan(
+                text: "Rating: ",
+                style: TextStyle(
+                fontSize: 12,
+                  )),
+                TextSpan(text: widget.book.rating)
+              ])),
+      ]),
+SizedBox(height: 8),
+      Row(children: [
+          RichText(
+           text: TextSpan(
+              style:
+              TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[              
+              TextSpan(
+                text: "Price: RM ",
+                style: TextStyle(
+                fontSize: 12,
+                  )),
+                TextSpan(text: widget.book.price)
+              ])),
+      ]),
+SizedBox(height: 8),
+      Row(children: [
+          RichText(
+           text: TextSpan(
+              style:
+              TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[              
+              TextSpan(
+                text: "Publisher: ",
+                style: TextStyle(
+                fontSize: 12,
+                  )),
+                TextSpan(text: widget.book.publisher)
+              ])),
+      ]),
+SizedBox(height: 8),
+      Row(children: [
+          RichText(
+           text: TextSpan(
+              style:
+              TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[              
+              TextSpan(
+                text: "ISBN: ",
+                style: TextStyle(
+                fontSize: 12,
+                  )),
+                TextSpan(text: widget.book.isbn)
+              ])),
+      ]),
+SizedBox(height: 10),
+      Row(children: [
+          RichText(
+           text: TextSpan(
+              style:
+              TextStyle(color: Colors.black, fontSize: 14),
+                children: <TextSpan>[              
+              TextSpan(
+                text: "Discrption:\n",
+                style: TextStyle(
+                fontSize: 14,
+                  )),
+                TextSpan(text: widget.book.description)
+              ])),
+      ]),
+      ])
+  )]
+  )
+  ),
+  )
+  );
+
+ } 
+}
---
+++ lib/mainscreen.dart
@@ -0,0 +1,150 @@
+import 'dart:convert';
+import 'package:book_depo/book.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:book_depo/bookdetailscreen.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  List bookList;
+  double screenHeight, screenWidth;
+  String _titlecenter = "Book Loading...."; 
+  
+
+  @override
+  void initState() {
+  super.initState();
+  _loadBook();
+      }
+  
+  @override
+    Widget build(BuildContext context) {
+      screenHeight = MediaQuery.of(context).size.height;
+      screenWidth = MediaQuery.of(context).size.width; 
+
+      return Scaffold(
+          appBar: AppBar(
+            title: Text('Main Page'),
+          ),
+          body: Column(
+            children:<Widget> [
+              bookList == null 
+              ? Flexible(
+                child:Container(
+                  child: Center(
+                    child: Text(
+                      _titlecenter,
+                      style: TextStyle(
+                        fontSize:18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black))))) 
+              : Flexible(
+                child: GridView.count(
+                crossAxisCount: 2,
+                childAspectRatio: (screenWidth / screenHeight) / 0.8,
+                children: List.generate(bookList.length, (index) {
+                    return Padding(
+                      padding: EdgeInsets.all(1),
+                      child: Card(
+                      child: InkWell(
+                      onTap: () => _loadBookDetail(index),
+                      child: SingleChildScrollView(
+                             child: Column(
+                               children: [
+                                Container(
+                                  height: screenHeight / 3.8,
+                                  width: screenWidth / 1.2,
+                                  child: CachedNetworkImage(
+                                  imageUrl:
+                                     "http://slumberjer.com/bookdepo/bookcover/${bookList[index]['cover']}.jpg",
+                                      fit: BoxFit.cover,
+                                      placeholder: (context, url) => 
+                                        new CircularProgressIndicator(),
+                                      errorWidget: (context, url, error) => 
+                                          new Icon(
+                                                Icons.broken_image,
+                                                size: screenWidth / 2,
+                                          ),
+                                          )), 
+                                          SizedBox(height: 5),
+                                          Text(
+                                           bookList[index]['booktitle'],
+                                            style: TextStyle(
+                                            fontSize: 16,
+                                              fontWeight: FontWeight.bold),
+                                                ),
+                                              Text(bookList[index]['author']),
+                                              Text(bookList[index]['description']),
+                                            ],
+                                          ),
+                                        ),
+                                      )));
+                                    }),
+                                  ))
+                                  ],
+                                  ),
+                                   );
+                                  }
+                            
+                          
+                        
+                      
+                      
+                      
+                      
+                      
+void _loadBook() {
+    http.post("http://slumberjer.com/bookdepo/php/load_books.php",
+    body: {}).then((res) {
+    print(res.body);
+    if (res.body == "nodata") {
+      bookList = null;
+      setState(() {
+      _titlecenter = "No Book Found";
+      });
+    } else {
+      setState(() {
+        var jsondata = json.decode(res.body);
+        bookList = jsondata["books"];
+      });
+      }
+      }).catchError((err) {
+        print(err);
+ });
+}
+                      
+_loadBookDetail(int index) async {
+  ProgressDialog pr = new ProgressDialog(context,
+        type: ProgressDialogType.Normal, isDismissible: false);
+    pr.style(message: "Loading...");
+    await pr.show();
+   print(bookList[index]['bookname']);
+    Book book = new Book(
+        bookid: bookList[index]['bookid'],
+        booktitle: bookList[index]['booktitle'],
+        author: bookList[index]['author'],
+        price: bookList[index]['price'],
+        description: bookList[index]['description'],
+        rating: bookList[index]['rating'],
+        publisher: bookList[index]['publisher'],
+        isbn: bookList[index]['isbn'],
+        cover: bookList[index]['cover']);
+
+        await pr.hide();
+
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => BookDetailScreen(
+                  book: book,
+                )));
+}
+}
